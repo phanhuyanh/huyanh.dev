697cfd359201dd8e23493b31d070912a94224223
huyanh <phanhuyanh251@gmail.com>
2021-03-29 14:38:26 +0700

    landing page MNTN

--- src/components/MNTN.vue
+++ src/components/MNTN.vue
@@ -25,9 +25,138 @@
         </div>
       </div>
       <div class="content-hero">
-        <div class="tag-line">
-          <div class="line"></div>
-          <div class="hiking"></div>
+        <div class="container">
+          <div class="content-wrapper">
+            <div class="tag-line">
+              <div class="line"></div>
+              <h5 class="hiking">A Hiking guide</h5>
+            </div>
+            <h1>Be prepared for the mountains and beyond!</h1>
+            <div class="scroll-down">
+              <span>scroll down</span>
+              <span class="scroll-down-icon">
+                <svg width="16" height="24" viewBox="0 0 16 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M16 16L14.59 14.59L9 20.17V0H7V20.17L1.42 14.58L0 16L8 24L16 16Z" fill="currentColor"></path>
+                </svg>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img class="mg" src="../assets/img/mntn/MG.png" data-rellax-speed="10">
+      <img class="hg" src="../assets/img/mntn/HG.png" data-rellax-speed="10">
+      <img class="vg" src="../assets/img/mntn/VG-p-500.png" data-rellax-speed="3">
+      <div class="gradient-hero"></div>
+    </div>
+    <div class="equipment section">
+      <div class="container">
+        <div class="section-wrapper">
+          <div class="section-content">
+            <div class="tag-line">
+              <div class="line"></div>
+              <h5 class="hiking">Get Started</h5>
+            </div>
+            <h2>What level of hiker are you?</h2>
+            <p>Determining what level of hiker you are can be an important tool when planning future hikes. This hiking level guide will help you plan hikes according to different hike ratings set by various websites like All Trails and Modern Hiker. What type of hiker are you – novice, moderate, advanced moderate, expert, or expert backpacker? </p>
+            <div class="read-more">
+              <span>read more</span>
+              <span>
+                <svg width="24" height="16" viewBox="0 0 24 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M16 -6.99382e-07L14.59 1.41L20.17 7L-3.93402e-07 7L-3.0598e-07 9L20.17 9L14.58 14.58L16 16L24 8L16 -6.99382e-07Z" fill="#FBD784"/>
+                </svg>
+              </span>
+            </div>
+            <div class="section-name">01</div>
+          </div>
+          <div class="section-img">
+            <img src="../assets/img/mntn/01.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="about-us section">
+      <div class="container">
+        <div class="section-wrapper reverse">
+          <div class="section-content">
+            <div class="tag-line">
+              <div class="line"></div>
+              <h5 class="hiking">Hiking Essentials</h5>
+            </div>
+            <h2>Picking the right Hiking Gear!</h2>
+            <p>The nice thing about beginning hiking is that you don’t really need any special gear, you can probably get away with things you already have. Let’s start with clothing. A typical mistake hiking beginners make is wearing jeans and regular clothes, which will get heavy and chafe wif they get sweaty or wet.</p>
+            <div class="read-more">
+              <span>read more</span>
+              <span>
+                <svg width="24" height="16" viewBox="0 0 24 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M16 -6.99382e-07L14.59 1.41L20.17 7L-3.93402e-07 7L-3.0598e-07 9L20.17 9L14.58 14.58L16 16L24 8L16 -6.99382e-07Z" fill="#FBD784"/>
+                </svg>
+              </span>
+            </div>
+            <div class="section-name">02</div>
+          </div>
+          <div class="section-img">
+            <img src="../assets/img/mntn/02.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="blog section">
+      <div class="container">
+        <div class="section-wrapper">
+          <div class="section-content">
+            <div class="tag-line">
+              <div class="line"></div>
+              <h5 class="hiking">Where you go is the key</h5>
+            </div>
+            <h2>Understand Your Map & Timing</h2>
+            <p>To start, print out the hiking guide and map. If it’s raining, throw them in a Zip-Lock bag. Read over the guide, study the map, and have a good idea of what to expect. I like to know what my next landmark is as I hike. For example, I’ll read the guide and know that say, in a mile, I make a right turn at the junction..</p>
+            <div class="read-more">
+              <span>read more</span>
+              <span>
+                <svg width="24" height="16" viewBox="0 0 24 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M16 -6.99382e-07L14.59 1.41L20.17 7L-3.93402e-07 7L-3.0598e-07 9L20.17 9L14.58 14.58L16 16L24 8L16 -6.99382e-07Z" fill="#FBD784"/>
+                </svg>
+              </span>
+            </div>
+            <div class="section-name">03</div>
+          </div>
+          <div class="section-img">
+            <img src="../assets/img/mntn/03.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer section">
+      <div class="container">
+        <div class="section-wrapper">
+          <div class="section-content">
+            <div class="logo">MNTN</div>
+            <p class="description">Get out there & discover your next slope, mountain & destination!</p>
+            <p class="copyright">Copyright 2019 MNTN, Inc. Terms & Privacy</p>
+            <p class="copyright">A concept by Kryston Schwarze</p>
+          </div>
+          <div class="section-img">
+            <div class="footer-menu">
+              <div class="more-blog">
+                <h5 class="title">More Blog</h5>
+                <ul>
+                  <li><a href="#">About MNTN</a></li>
+                  <li><a href="#">Contributors & Writers</a></li>
+                  <li><a href="#">Write For Us</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                  <li><a href="#">Privacy Policy</a></li>
+                </ul>
+              </div>
+              <div class="more-landing-page">
+                <h5 class="title">More Landing Page</h5>
+                <ul>
+                  <li><a href="#">The team</a></li>
+                  <li><a href="#">Jobs</a></li>
+                  <li><a href="#">Press</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -35,13 +164,43 @@
 </template>
 
 <script>
+import rellax from 'rellax'
 export default {
+  mounted() {
+    this.parallax()
+  },
+  methods: {
+    parallax() {
+      new rellax('.mg')
+      new rellax('.vg')
+      new rellax('.hg')
+
+      let contentHero = this.$el.querySelector('.content-hero')
+      window.addEventListener('scroll', () => {
+        let pageYOffset = window.pageYOffset
+        let value = pageYOffset / (window.innerHeight * .51) * 100
+        contentHero.style.opacity = (100 - value) + '%'
+      })
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
 @import url('http://fonts.cdnfonts.com/css/chronicle-display');
 $color-link-hover: #fbd784;
+$mobile: 768px;
+
+@font-face {
+  font-family: serifDisplaySemiBold;
+  src: url('../assets/font/SerifDisplay-Semibold.otf') format("opentype");
+}
+
+@mixin mobile {
+  @media screen and (max-width: $mobile){
+    @content;
+  }
+}
 
 .MNTN {
   font-family: 'Sans Serif', Arial, sans-serif;
@@ -62,6 +221,8 @@ $color-link-hover: #fbd784;
   align-items: center;
   color: #fff;
   padding-top: 40px;
+  position: relative;
+  z-index: 10;
 }
 .container {
   padding: 0 80px;
@@ -76,12 +237,12 @@ $color-link-hover: #fbd784;
 .justify-content-between {
   justify-content: space-between;
 }
-.header .logo {
+.logo {
   font-size: 32px;
   line-height: 36px;
   font-family: 'Chronicle Display', sans-serif;
 }
-.header .logo a {
+.logo a {
   text-decoration: none;
   color: #fff;
   letter-spacing: 0.01em;
@@ -89,6 +250,9 @@ $color-link-hover: #fbd784;
 .header .nav {
   list-style: none;
   display: flex;
+  @include mobile() {
+    display: none;
+  }
 }
 .header .nav li {
   display: flex;
@@ -127,5 +291,244 @@ $color-link-hover: #fbd784;
 }
 .header .account .text {
   font-weight: 700;
+}
+.tag-line {
+  display: flex;
+  align-items: center;
+  margin-bottom: 32px;
+}
+.line {
+  width: 72px;
+  height: 2px;
+  background: $color-link-hover;
+  margin-right: 24px;
+}
+.hiking {
+  font-size: 14px;
+  line-height: 24px;
+  color: $color-link-hover;
+  font-weight: 800;
+  letter-spacing: 6px;
+  text-transform: uppercase;
+  }
+h1, h2 {
+  font-size: 75px;
+  line-height: 120%;
+  font-weight: 400;
+  text-transform: capitalize;
+  margin-bottom: 32px;
+  font-family: serifDisplaySemiBold;
+}
+.scroll-down, .read-more {
+  display: flex;
+  align-items: center;
+  max-width: max-content;
+  transition: 200ms ease all;
+  cursor: pointer;
+  &:hover span {
+    color: $color-link-hover;
+    svg {
+      path {
+        fill: $color-link-hover;
+      }
+    }
+  }
+  & span:first-child {
+    font-weight: 700;
+  }
+}
+.scroll-down-icon {
+  margin-left: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.content-hero {
+  position: fixed;
+  left: 0;
+  top: 25%;
+  width: 100%;
+  z-index: 10;
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .content-wrapper {
+    width: 100%;
+    max-width: 790px;
+  }
+}
+.mg {
+  position: absolute;
+  top: 25%;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+img {
+  max-width: 100%;
+  vertical-align: middle;
+  display: inline-block;
+}
+.vg {
+  position: absolute;
+  top: 50%;
+  left: 0%;
+  width: 100%;
+  height: 100%;
+  z-index: 50;
+}
+.hg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+.gradient-hero {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(135deg, #0b1d26, transparent 87%);
+  opacity: .6;
+}
+
+.section {
+  position: relative;
+  background: linear-gradient(180deg, #0b1d26, #0b1d26 20%);
+  z-index: 50;
+  height: 100vh;
+  &:first-child {
+    background: linear-gradient(180deg, rgba(11, 29, 38, 0), #0b1d26 20%);
+    padding-top: 200px;
+  }
+  .section-wrapper.reverse {
+    flex-direction: row-reverse;
+    .section-img {
+      justify-content: flex-start;
+    }
+    @include mobile {
+      flex-direction: column-reverse;
+    }
+  }
+  .section-wrapper {
+    width: 100%;
+    max-width: 1440px;
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
+    @include mobile() {
+      flex-direction: column-reverse;
+    }
+  }
+  h2 {
+    font-size: 64px;
+    line-height: 74px; 
+  }
+  .section-content {
+    flex-basis: 40%;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+  }
+  .section-img {
+    flex-basis: 60%;
+    display: flex;
+    justify-content: flex-end;
+    margin-bottom: 80px;
+  }
+  p {
+    font-size: 18px;
+    line-height: 32px;
+    margin: 27px 0;
+    font-weight: normal;
+  }
+  .read-more { 
+    color: $color-link-hover;
+    font-size: 18px;
+    line-height: 22px;
+    span:last-child {
+      display: flex;
+      align-items: center;
+      justify-items: center;
+      margin-left: 24px;
+    }
+  }
+  .section-name {
+    position: absolute;
+    font-size: 240px;
+    opacity: .1;
+    line-height: 240px;
+    top: -15%;
+    font-weight: bold;
+    left: -21%;
+    color: #fff;
+    @include mobile {
+      left: -12%;
+    }
+  }
+}
+
+.footer {
+  height: initial;
+  padding-bottom: 50px;
+  @include mobile {
+    padding-bottom: 0;
+  }
+  .description, .copyright {
+    font-size: 18px;
+    line-height: 32px;
+    width: 303px;
+  }
+  .copyright {
+    width: unset;
+    opacity: .5;
+    margin: 0;
+    font-size: 15px;
+    margin-top: auto;
+  }
+  .footer-menu {
+    width: 556px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .more-blog, .more-landing-page {
+    ul {
+      list-style: none;
+      li {
+        a {
+          text-decoration: none;
+          color: #fff;
+          font-size: 15px;
+          line-height: 24px;
+          font-weight: 400;
+        }
+      }
+    }
+    h5.title {
+      font-size: 24px;
+      line-height: 32px;
+      color: $color-link-hover;
+      margin-bottom: 15px;
+    }
+  }
+  .more-landing-page {
+    @include mobile {
+      display: none;
+    }
+  }
+  .section-wrapper {
+    @include mobile {
+      flex-direction: column;
+    }
+  }
+  .section-img {
+    margin-top: 60px;
+    margin-bottom: 60px;
+  }
 }
 </style>
